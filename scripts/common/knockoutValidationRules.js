3259d3112e9c75fd6e85d7b0feefd4b410ba6909
Hasmik Baghdasaryan <hasmikbaghdasaryan92@gmail.com>
2024-02-16 16:50:41 +0400

    personal information update functionality and validation added

---
+++ scripts/common/knockoutValidationRules.js
@@ -0,0 +1,31 @@
+ko.validation.init({
+    errorMessageClass: 'koErrorMessage',
+    errorElementClass: 'koErrorField',
+    decorateInputElement: true,
+  });
+
+  ko.validation.rules['emailAddressFormatValidation'] = {
+    validator: function (email) {
+      var regExp = new RegExp( /^[\w\.\-]+@[\w]+\.[a-zA-Z]{2,4}$/gm);
+      return regExp.test(email) ? true : false;
+    },
+    message: 'Invalid format'
+  };
+
+  ko.validation.rules['ifOnlyText']={
+    validator: function(value) {
+      var regExp = new RegExp(/^[A-Za-z ]+$/);
+      return regExp.test(value) ? true : false;
+    },
+    message: 'Only letters allowed'
+  };
+
+  ko.validation.rules['ifOnlyDigits'] = {
+    validator: function(value) {
+      var regExp = new RegExp(/^[\d]+$/);
+      return regExp.test(value) ? true : false;
+    },
+    message: 'Only digits allowed'
+  };
+  
+  ko.validation.registerExtenders();
--- scripts/viewModels/PersonalInformationViewModel.js
+++ scripts/viewModels/PersonalInformationViewModel.js
@@ -1,22 +1,73 @@
 var PersonalInformationViewModelKO;
 function PersonalInformationViewModel(){
     var self = this;
-    self.data = HeaderViewModelKO.dataFromServer;
+    var server = ServerStub();
+    self.info = HeaderViewModelKO.dataFromServer;
+    self.data = ko.observable(self.info);
     self.showData = HomeViewModelKO.showData;
-    self.personalData = self.data && self.data.personal ? self.data.personal : null;
-    self.personalInformation = {
-        firstName: ko.observable(self.personalData && self.personalData.firstName ? self.personalData.firstName : ''),
-        lastName: ko.observable(self.personalData && self.personalData.lastName ? self.personalData.lastName : ''),
-        phoneNumber: ko.observable(self.personalData && self.personalData.phoneNumber ? self.personalData.phoneNumber : ''),
-        emailAddress: ko.observable(self.personalData && self.personalData.emailAddress ? self.personalData.emailAddress : ''),
-        address: 
-            {
-                city: ko.observable(self.personalData && self.personalData.address.city ? self.personalData.address.city : ''),
-                country: ko.observable(self.personalData && self.personalData.address.country ? self.personalData.address.country : ''),
-                street: ko.observable(self.personalData && self.personalData.address.street ? self.personalData.address.street : ''),
-                postCode: ko.observable(self.personalData && self.personalData.address.postCode ? self.personalData.address.postCode : '')
+    self.personalData = self.data() && self.data().personal ? self.data().personal : null;
+    self.showChangeMessage=ko.observable(false);
+    self.editPersonalInformation = ko.observable(false);
+
+    self.firstName = ko.observable().extend({required: true, ifOnlyText: self.firstName}),
+    self.firstName(self.personalData && self.personalData.firstName ? self.personalData.firstName : '');
+
+    self.lastName =  ko.observable().extend({required: true, ifOnlyText: self.lastName}),
+    self.lastName(self.personalData && self.personalData.lastName ? self.personalData.lastName : '');
+
+    self.phoneNumber = ko.observable().extend({required: true, ifOnlyDigits: self.phoneNumber,  minLength: 4, maxLength: 9}),
+    self.phoneNumber(self.personalData && self.personalData.phoneNumber ? self.personalData.phoneNumber : ''),
+
+    self.emailAddress = ko.observable().extend({required: true, emailAddressFormatValidation: self.emailAddress})
+    self.emailAddress(self.personalData && self.personalData.emailAddress ? self.personalData.emailAddress : ''),
+
+    self.city = ko.observable().extend({required: true, ifOnlyText: self.city}),
+    self.city(self.personalData && self.personalData.address.city ? self.personalData.address.city : '');
+
+    self.country = ko.observable().extend({required: true, ifOnlyText: self.country}),
+    self.country(self.personalData && self.personalData.address.country ? self.personalData.address.country : '');
+
+    self.street = ko.observable().extend({required: true}),
+    self.street(self.personalData && self.personalData.address.street ? self.personalData.address.street : '');
+
+    self.postCode = ko.observable().extend({required: true, ifOnlyDigits: self.postCode, maxLength: 4});
+    self.postCode((self.personalData && self.personalData.address.postCode ? self.personalData.address.postCode : ''));
+
+    self.personalInformation = ko.pureComputed(function(){
+        return {
+            firstName: self.firstName(),
+            lastName: self.lastName(),
+            contactDetails: {
+                phoneNumber: self.phoneNumber(),
+                emailAddress: self.emailAddress(),
+            },
+            address: {
+                city: self.city(),
+                country: self.country(),
+                street: self.street(),
+                postCode: self.postCode()
             }
+        }
+    });
+ 
+    self.enableEditPersonalInfo = function(){
+        self.editPersonalInformation(true);
     };
+    
+    self.disableEditPersonalInfo = function(){
+        self.editPersonalInformation(false);
+    };
+
+    self.updatePersonalInfo = function(){
+        server.updatePersonalInformation((self.personalInformation()));
+        self.editPersonalInformation(false);
+        self.showChangeMessage(true);
+    };
+
+    self.errors = ko.validation.group(self, {deep: true, observable: true, live: true});
+    self.isSubmitBtnEnabled = function(){
+        return !self.errors().length ? true : false;
+    }
 };
 
 function initPersonalInformationViewModel(){
